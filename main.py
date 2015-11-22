40665f548e91ddcadd989d5cfaaa402a98c3ee57
rupakraj <rughimire@gmail.com>
2015-11-22 11:42:05 +0545

    Initial Commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import yaml
+
+
+def check_todo():
+    with open("data/TODO.yaml", 'r') as todo_file:
+        todo = yaml.load(todo_file)
+
+    for type_name in todo:
+        # print(types)
+        message = ""
+
+        for task in todo[type_name]:
+            message += "* "+task+"\n"
+            #print (task)
+
+        notify(title=type_name, message=message.strip())
+
+def notify(message, title='my-reminder', icon='dialog-information'):
+    os.system('notify-send "'+title+'" "'+message+'" --icon='+icon)
+
+
+# def check_todo():
+#     f = open("TODO.yaml")
+#     for line in  f.readlines():
+#         print(line.strip())
+
+
+if __name__ =="__main__":
+    check_todo()
+    # notify(message='I am from my-reminder')
+    # notify(message='I am from my-reminder \n Multiline Enabled')
+    # notify(message='I am error message', icon='dialog-error')
---
+++ readme.md
@@ -0,0 +1,25 @@
+# my-reminder
+Easy reminder/notification application with __<3__
+* __Note:__ Incomplete __:(__
+
+## Installation
+Just clone the sources
+```
+$ git clone https://github.com/rupakraj/my-reminder.git /path/to/your/home
+```
+
+### Make sample data
+The data is plain text file in the format of **yaml**. Sample file is included in the _data/TODO-sample.yaml_
+To create your own just do cp magic
+```
+$ cp /data/TODO-sample.yaml /data/TODO.yaml
+```
+
+### Run the application
+We need python3 to run the application
+```
+$ python3 main.py
+```
+
+## TODO
+* Can't list now- its huge __:(__
